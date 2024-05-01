07eab621acf9d126bfdc5f126bac17803648b077
Henrique Custódia <martinshenrique8@gmail.com>
2024-05-01 15:18:07 -0300

    feat: adiciona exemplo usando um validador async

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,58 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  Validators,
+} from "@angular/forms";
+import { delay, Observable, of } from "rxjs";
+
+function isUsernameAlreadyTakenValidator(control: AbstractControl): Observable<ValidationErrors | null> {
+  let validationError: ValidationErrors | null = null;
+
+  if (control.value === "henrique") {
+    validationError = { usernameIsAlreadyTaken: true };
+  }
+
+  return of(validationError).pipe(delay(500));
+}
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule, CommonModule],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <form [formGroup]="form">
+      <fieldset>
+        <input
+          type="text"
+          formControlName="username"
+          placeholder="Digite seu @username"
+        />
 
-    <router-outlet />
+        @if (form.get('username')?.touched) {
+          @if (form.get('username')?.hasError('required')) {
+            <div [ngStyle]="{ color: 'red' }">Por favor, informe um username.</div>
+          } 
+          
+          @if (form.get('username')?.hasError('usernameIsAlreadyTaken')) {
+            <div  [ngStyle]="{ color: 'red' }">O username informado já existe.</div>
+          }
+        }
+      </fieldset>
+    </form>
   `,
   styles: [],
 })
 export class AppComponent {
-  title = 'angular-form-custom-validators';
+  form = new FormGroup({
+    username: new FormControl(
+      "",
+      Validators.required,
+      isUsernameAlreadyTakenValidator
+    ),
+  });
 }
